5756d0579bcb0b2e5b87a95cedce8267fed4a729
Foyez-Ahamed <foyez1435@gmail.com>
2023-11-24 15:55:00 +0600

    import poppins font style from google fonts

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <h1 className='text-center'>Hello world</h1>
+      <h1 className='text-center font-poppins text-3xl font-bold'>Hello world</h1>
     </>
   )
 }
